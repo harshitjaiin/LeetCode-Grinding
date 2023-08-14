e7ff9535db2eb9ad2066d5e5dfefb6aed5509736
Harshit Jain <harshitjain.tj@gmail.com>
2023-08-15 01:49:30 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (6.27%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
-    vector<int> getRow(int numRows) {
-        
-    
-        vector<vector<int>> ans;
-        ans.push_back({1});
-        if(numRows==0) return {1};
-        
-        for(int sz = 2 ; sz<=numRows+1 ;sz++){
-            
-            vector<int> temp(sz , -1);
-            temp[0]=1;
-            temp[sz-1]=1;
-
-            for(int i = 1 ; i<sz-1 ; i++){
-                
-               temp[i]=ans[sz-2][i-1] + ans[sz-2][i]; 
-                
-            }
-            
-            if(sz==numRows+1){
-                return temp;
-            }
-            // cout<<endl;
-            ans.push_back(temp);
-            
+    vector<int> getRow(int rowIndex) {
+        if(rowIndex==0){
+            return {1};
         }
-        return {};
+        
+        vector<int> currRow = {1};
+        //pichle wala row ko nikal do
+        vector<int> prevRow = getRow(rowIndex-1);
+        
+        for(int i = 1 ; i<rowIndex ; i++){
+            currRow.push_back(prevRow[i-1]+prevRow[i]);
+        }
+        
+        currRow.push_back(1);
+        return currRow;
     }
 };
