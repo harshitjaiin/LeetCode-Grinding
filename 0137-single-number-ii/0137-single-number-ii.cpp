4efb46ea0a339d12847d57ee20c1ce90c4bd2f3f
Harshit Jain <harshitjain.tj@gmail.com>
2023-08-16 15:03:43 +0530

    Time: 2 ms (98.09%), Space: 9.4 MB (66.10%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -7,11 +7,14 @@ public:
             int sum = 0;
             
             for(auto it : nums){
+                //har ele ki i times right shift krke last value leke uska sum kr rahe (& 1 means ki jo wo bit hai woi ans aajaye) 
+                //&1 ke jagah we can also use (|0) too;
                 sum+=(it>>i)&1;
             }
             
+            //jo value apne unique ele ki hogi wo hi ek triplet ko kharab kregi toh apan div by 3 krke apne unique number ki bit nikal rahe hai
             sum=sum%3;
-            
+            //iske bad apan peeche se nikal rahe islei ans me or krke sum ko left shift krke uski shi jagah pe dal re i means bit position idhar!!    
             ans = ans | sum<<i;
         }
         
