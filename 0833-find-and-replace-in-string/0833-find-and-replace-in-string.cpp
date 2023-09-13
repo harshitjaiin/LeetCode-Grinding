771101ebd8ba565b25fc6a803359c5be7bbaddd8
Harshit Jain <harshitjain.tj@gmail.com>
2023-09-13 21:45:42 +0530

    Time: 8 ms (16.40%), Space: 11.3 MB (35.67%) - LeetHub

---
+++ 0833-find-and-replace-in-string/0833-find-and-replace-in-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string findReplaceString(string s, vector<int>& indices, vector<string>& sources, vector<string>& targets) {
+        
+       map<int,pair<string,string>>hash;
+        
+        int n=indices.size();
+        int i;
+        for(i=0;i<n;i++){
+            int start_idx=indices[i];
+            string have=sources[i];
+            string convert=targets[i];
+            
+            if(s.substr(start_idx,have.size())==have)
+            hash[start_idx]={have,convert};
+        }
+
+        int start=0;
+        string res="";
+        
+        while(start<s.size())
+        {   
+            //replace wali index hai kya??
+            if(hash.find(start)!=hash.end()){
+                res+=hash[start].second;
+                start+=hash[start].first.size();
+            }
+            else{
+                //simple add
+                res+=s[start];
+                start++;   
+            }
+        }
+        
+        return res;
+    }
+};
